fac19200a33f34e5f9a3cbe5bb5b89a34a61e326
Daria Duminska <md.apathy@gmail.com>
2020-06-02 12:34:12 +0300

    fix url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/api/',
-  //apiUrl:"https://ultimate-task-list.herokuapp.com/api/"
+  //apiUrl: 'http://localhost:8081/api/',
+  apiUrl:"https://ultimate-task-list.herokuapp.com/api/"
 };
 
 /*
